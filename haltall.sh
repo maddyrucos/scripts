cb5b3282e67cb6fad567f9e5652d5972c45b631b
igor <maddyrucos@icloud.com>
2024-10-09 22:37:28 +0300

    Created haltall script

---
+++ haltall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Enter hostnames of your clients 
+# DO NOT ENTER SERVER THERE. IT HALTS LATER
+hosts='host1 host2'
+
+
+for host in $hosts 
+do
+	ssh $host 'sync; halt'
+done
+
+halt
